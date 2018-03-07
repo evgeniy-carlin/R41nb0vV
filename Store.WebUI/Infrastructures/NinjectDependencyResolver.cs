b5216f8dcaaf93dd8c728b623618c6315b26d99e
Eugene <c00p.g4ming@gmail.com>
2018-03-07 14:16:42 +0200

    start project Store

---
+++ Store.WebUI/Infrastructures/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Moq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.Infrastructures
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        public void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Nike", Price = 1299},
+                new Product {Name = "Puma", Price = 699},
+                new Product {Name = "Adidas", Price = 900}
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
